d4f2d8facf86c8543cb42f6dd304d625de5de5bc
Revanth1902 <revanth19a@gmail.com>
2025-09-13 17:04:51 +0530

    added test otp

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import AuthWithOTP from "./components/AuthForm";
 import Home from "./components/Homepage";
 import ProtectedRoute from "./ProtectedRoute";
-import LandindgPage from "./components/LandingPage";
+import LandindgPage from "./components/Dashboard";
 
 function App() {
   return (
